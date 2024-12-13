fd02623de24f410638eb5ecfcbac89e0ad4ff3f0
yellowhat <yellowhat46@gmail.com>
2024-12-13 07:17:28 +0000

    kubernetes: 1.32.0

--- packages/kubernetes/default.nix
+++ packages/kubernetes/default.nix
@@ -21,13 +21,13 @@
 
 buildGoModule rec {
   pname = "kubernetes";
-  version = "1.31.4";
+  version = "1.32.0";
 
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "kubernetes";
     rev = "v${version}";
-    hash = "sha256-XEilva/K2xGZHhrifaK/f4a3PGPb5dClOqv1dlJOTCM=";
+    hash = "sha256-VpinMMWvFYpcqDC9f3q/oEqUHRz7thHMs0bKt6AaNms=";
   };
 
   vendorHash = null;
